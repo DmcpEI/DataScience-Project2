5aced8f850e42af8187543ed51bdc18aead585c0
ist198933 <fabioribeiro2002@tecnico.ulisboa.pt>
2024-12-07 22:33:32 +0000

    add Random Forests

--- classes/DataModeling.py
+++ classes/DataModeling.py
@@ -1,16 +1,18 @@
 from matplotlib.figure import Figure
+from matplotlib.pyplot import figure, savefig, show, title, imshow, imread, axis, subplots
+from typing import Literal
 from numpy import array, ndarray, argsort, arange
 from sklearn.metrics import confusion_matrix
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
-from matplotlib.pyplot import figure, savefig, show, title, imshow, imread, axis, subplots
-from typing import Literal
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier, export_graphviz, plot_tree
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
 from subprocess import call
 
 from dslabs_functions import CLASS_EVAL_METRICS, DELTA_IMPROVE, plot_bar_chart, \
-    plot_multiline_chart, plot_horizontal_bar_chart, HEIGHT, plot_line_chart, plot_multibar_chart, plot_confusion_matrix
+    plot_multiline_chart, plot_horizontal_bar_chart, HEIGHT, plot_line_chart, plot_multibar_chart, plot_confusion_matrix, \
+    CLASS_EVAL_METRICS, DELTA_IMPROVE, read_train_test_from_files
 
 
 class DataModeling:
@@ -545,3 +547,84 @@ class DataModeling:
             percentage=False,
         )
         savefig(f"graphs/data_modeling/mlp/{self.data_loader.file_tag}_mlp_{eval_metric}_loss_curve.png")
+
+    def random_forests_study(self,
+        trnX: ndarray,
+        trnY: array,
+        tstX: ndarray,
+        tstY: array,
+        nr_max_trees: int = 2500,
+        lag: int = 500,
+        metric: str = "accuracy",
+    ) -> tuple[RandomForestClassifier | None, dict]:
+        n_estimators: list[int] = [100] + [i for i in range(500, nr_max_trees + 1, lag)]
+        max_depths: list[int] = [2, 5, 7]
+        max_features: list[float] = [0.1, 0.3, 0.5, 0.7, 0.9]
+
+        best_model: RandomForestClassifier | None = None
+        best_params: dict = {"name": "RF", "metric": metric, "params": ()}
+        best_performance: float = 0.0
+
+        values: dict = {}
+
+        cols: int = len(max_depths)
+        _, axs = subplots(1, cols, figsize=(cols * HEIGHT, HEIGHT), squeeze=False)
+        for i in range(len(max_depths)):
+            d: int = max_depths[i]
+            values = {}
+            for f in max_features:
+                y_tst_values: list[float] = []
+                for n in n_estimators:
+                    clf = RandomForestClassifier(
+                        n_estimators=n, max_depth=d, max_features=f
+                    )
+                    clf.fit(trnX, trnY)
+                    prdY: array = clf.predict(tstX)
+                    eval: float = CLASS_EVAL_METRICS[metric](tstY, prdY)
+                    y_tst_values.append(eval)
+                    if eval - best_performance > DELTA_IMPROVE:
+                        best_performance = eval
+                        best_params["params"] = (d, f, n)
+                        best_model = clf
+                    # print(f'RF d={d} f={f} n={n}')
+                values[f] = y_tst_values
+            plot_multiline_chart(
+                n_estimators,
+                values,
+                ax=axs[0, i],
+                title=f"Random Forests with max_depth={d}",
+                xlabel="nr estimators",
+                ylabel=metric,
+                percentage=True,
+            )
+        print(
+            f'RF best for {best_params["params"][2]} trees (d={best_params["params"][0]} and f={best_params["params"][1]})'
+        )
+        return best_model, best_params
+
+    def random_forests(self):
+        eval_metric = "accuracy"
+
+        trnX: ndarray = self.X_train.values
+        tstX: ndarray = self.X_test.values
+        trnY: array = self.y_train.values
+        tstY: array = self.y_test.values
+        labels: list = list(self.y_train.unique())
+        labels.sort()
+        vars = self.X_train.columns.to_list()
+
+        print(f"Train#={len(trnX)} Test#={len(tstX)}")
+        print(f"Labels={labels}")
+
+        figure()
+        best_model, params = self.random_forests_study(
+            trnX,
+            trnY,
+            tstX,
+            tstY,
+            nr_max_trees=1000,
+            lag=250,
+            metric=eval_metric,
+        )
+        savefig(f"graphs/data_modeling/rf/{self.data_loader.file_tag}_rf_{eval_metric}_study.png")
+        show()
